6853c4c7b6fc4248860bb476d35a962134863186
HaoWang95 <alanwang0028@gmail.com>
2024-09-25 11:33:33 +1000

    separate files

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- markdown = { 'markdownlint' },
         -- html = { 'tidy' },
         -- python = { 'flake8' },
       }
