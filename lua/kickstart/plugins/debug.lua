c3a74c4e54407d7d7cd55c0ca9e4af8f6a05c17a
HaoWang95 <alanwang0028@gmail.com>
2025-05-12 14:00:01 +1000

    fix lsp breaking

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -18,7 +18,7 @@ return {
     'nvim-neotest/nvim-nio',
 
     -- Installs the debug adapters for you
-    'williamboman/mason.nvim',
+    -- 'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
 
     -- Add your own debuggers here
